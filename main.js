d57ada5c9aff696ec4d44ade7dc7ff5a16208d2a
văn vui vẻ <haytv2222@gmail.com>
2024-05-07 15:03:19 +0700

    updata music

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const backgroundImg = document.querySelector('.background-img')
+const backgroundcolor = document.querySelector('.control')
 const nameSong = document.querySelector('.control_container-name')
 const audio = document.getElementById('myAudio')
 
@@ -7,64 +8,132 @@ const app = {
     isPlaying: false,
     song:[
         {
-            namesong: 'Fall back',
+            namesong: 'Sớm Mai',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Lithe - Fall Back (Visualizer) (128 kbps).mp3',
-            lickimg: './acset/object_music/1.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Sớm Mai - PiaLinh, Ronboogz, BONN!EX (Official Music Video) (320 kbps).mp3',
+            lickimg: './acset/object_music/1.jpg',
+            backgroundcolor:'linear-gradient(45deg, #28261b, #0c130f, #dec2b0, #ee7d20)',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'Lụy Tình',
+            namesong: 'Quá Sớm',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Lụy Tình - Hoàng Lụt (prod. NEMTRAN) _ SPEED UP (128 kbps).mp3',
-            lickimg: './acset/object_music/2.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Quá Sớm _ Low G _ Rap Nhà Làm (320 kbps).mp3',
+            lickimg: './acset/object_music/2.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(26 32 44), rgb(76 152 199), rgb(3 9 29), rgb(106 78 80))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'mai',
+            namesong: 'SỐNG CHO HẾT ĐỜI THANH XUÂN',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - mai - jimmi ngủyên x michael hư đốn (speedup + reverb) (128 kbps).mp3',
-            lickimg: './acset/object_music/3.jpg'
+            linkmusic: './acset/object_music/y2meta.com - SỐNG CHO HẾT ĐỜI THANH XUÂN _ MUSIC VIDEO _ Dick x Xám x Tuyết (320 kbps).mp3',
+            lickimg: './acset/object_music/3.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(89 83 87), rgb(156 84 47), rgb(34 23 19), rgb(154 84 50))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'Ngỏ Lời',
+            namesong: 'Thằng Mike',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Ngỏ Lời (prod.Kuyo) (128 kbps).mp3',
-            lickimg: './acset/object_music/4.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Thằng Mike (Audio) (320 kbps).mp3',
+            lickimg: './acset/object_music/4.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(88 96 110), rgb(30 35 48), rgb(50 58 24), rgb(45 26 24))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'PARACHUTE',
+            namesong: 'Trời ơi con chưa muốn chết',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - PARACHUTE - PARYS (feat. Ivy) _ SPEED UP (128 kbps).mp3',
-            lickimg: './acset/object_music/5.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Đen - Trời ơi con chưa muốn chết (Prod. by Tantu Beats) (320 kbps).mp3',
+            lickimg: './acset/object_music/5.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(217 217 217), rgb(14 14 14), rgb(218 218 218), rgb(150 150 150))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'TÌNH YÊU CÓ NGHĨA LÀ EM 2',
+            namesong: 'Lộn Xộn ll',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - TÌNH YÊU CÓ NGHĨA LÀ EM 2 _ [ Mono x Tlinh ] - DangLil (128 kbps).mp3',
-            lickimg: './acset/object_music/6.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Đen - Lộn Xộn ll (320 kbps).mp3',
+            lickimg: './acset/object_music/6.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(50 55 48), rgb(21 24 31), rgb(58 65 79), rgb(88 69 76))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'Gửi mẹ',
+            namesong: 'Trời hôm nay nhiều mây cực!',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Gửi mẹ - Nger (Prod by Shark) _ Lyrics video _ HD1080p _ Video Fanmade (128 kbps).mp3',
-            lickimg: './acset/object_music/7.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Trời hôm nay nhiều mây cực! (320 kbps).mp3',
+            lickimg: './acset/object_music/7.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(138 137 186), rgb(145 138 190), rgb(92 128 181), rgb(138 132 178))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'Tình yêu màu nắng',
+            namesong: '_Youngz_',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Tình yêu màu nắng (prod.Kuyo) (128 kbps).mp3',
-            lickimg: './acset/object_music/8.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Wxrdie - _Youngz_ ft. Tommy Tèo [Prod. by wokeupat4am] (320 kbps).mp3',
+            lickimg: './acset/object_music/8.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(46 62 9), rgb(45 152 40), rgb(118 74 103), rgb(137 11 180))',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'Vacole',
+            namesong: ' _KARMA_',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Vacole - Parys _ LYRICS _ SPEED UP (128 kbps).mp3',
-            lickimg: './acset/object_music/9.jpg'
+            linkmusic: './acset/object_music/y2meta.com - willistic - _KARMA_ ft. Tommy Tèo _ OFFICIAL AUDIO (320 kbps).mp3',
+            lickimg: './acset/object_music/9.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(182 184 183), rgb(54 70 83), rgb(133 142 147)',
+            backgroundsize:'400% 400%'
         },
         {
-            namesong: 'Yêu Kiều',
+            namesong: 'Rolling',
             singer: 'Văn vui vẻ',
-            linkmusic: './acset/object_music/y2meta.com - Yêu Kiều - Parys (speedup + reverb) (128 kbps).mp3',
-            lickimg: './acset/object_music/10_.jpg'
+            linkmusic: './acset/object_music/y2meta.com - Richie D. ICY & AXTROBOY - Rolling (Official Music Video) (320 kbps).mp3',
+            lickimg: './acset/object_music/10.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(14 15 11), rgb(63 69 45), rgb(11 10 12), rgb(75 103 112))',
+            backgroundsize:'400% 400%'
+        },
+        {
+            namesong: 'SỐNG CHO HẾT ĐỜI THANH XUÂN 2',
+            singer: 'Văn vui vẻ',
+            linkmusic: './acset/object_music/y2meta.com - SỐNG CHO HẾT ĐỜI THANH XUÂN 2 - BCTM x TNS  ( Official Music Video ) (320 kbps).mp3',
+            lickimg: './acset/object_music/11.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(255 253 225), rgb(180 189 126), rgb(150 161 107), rgb(255 254 223))',
+            backgroundsize:'400% 400%'
+        },
+        {
+            namesong: 'ghệ iu dấu của em ơi',
+            singer: 'Văn vui vẻ',
+            linkmusic: './acset/object_music/y2meta.com - tlinh - ghệ iu dấu của em ơi (feat. 2pillz, wokeup) _ OFFICIAL MUSIC VIDEO (320 kbps).mp3',
+            lickimg: './acset/object_music/12.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(146 81 67), rgb(246 82 174), rgb(137 37 68), rgb(231 203 187))',
+            backgroundsize:'400% 400%'
+        },
+        {
+            namesong: 'nếu lúc đó',
+            singer: 'Văn vui vẻ',
+            linkmusic: './acset/object_music/y2meta.com - tlinh - nếu lúc đó (ft. 2pillz) _ OFFICIAL MUSIC VIDEO (320 kbps).mp3',
+            lickimg: './acset/object_music/13.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(142 92 175), rgb(11 19 22), rgb(179 38 95), rgb(0 1 5))',
+            backgroundsize:'400% 400%'
+        },
+        {
+            namesong: 'TẠI VÌ SAO',
+            singer: 'Văn vui vẻ',
+            linkmusic: './acset/object_music/y2meta.com - RPT MCK - TẠI VÌ SAO _ Official Music Video (320 kbps).mp3',
+            lickimg: './acset/object_music/14.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(254 254 254), rgb(5 5 5), rgb(199 11 6), rgb(155 155 155))',
+            backgroundsize:'400% 400%'
+        },
+        {
+            namesong: 'Waiting For You',
+            singer: 'Văn vui vẻ',
+            linkmusic: './acset/object_music/y2meta.com - MONO - Waiting For You (Album 22 - Track No.10) (320 kbps).mp3',
+            lickimg: './acset/object_music/15.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(27 46 42), rgb(218 74 51), rgb(209 69 58), rgb(27 32 28))',
+            backgroundsize:'400% 400%'
+        },
+        {
+            namesong: 'Có khóc cũng thế thôi',
+            singer: 'Văn vui vẻ',
+            linkmusic: './acset/object_music/y2meta.com - Hào _ Có khóc cũng thế thôi (Official Visualizer) (320 kbps).mp3',
+            lickimg: './acset/object_music/16.jpg',
+            backgroundcolor:'linear-gradient(45deg, rgb(108 104 33), rgb(0 0 0), rgb(19 21 17), rgb(216 4 5))',
+            backgroundsize:'400% 400%'
         },
     ],
     
@@ -225,6 +294,8 @@ const app = {
         number = (number + 1) % this_.song.length; 
             const array = this_.song[number]
             backgroundImg.style.backgroundImage = "url('" + array.lickimg + "')";
+            backgroundcolor.style.background = array.backgroundcolor;
+            backgroundcolor.style.backgroundSize = array.backgroundsize;
             nameSong.textContent = array.namesong
             audio.src = array.linkmusic 
             audio.play()
@@ -235,8 +306,10 @@ const app = {
        const back = document.querySelector('.back_song')
        back.addEventListener('click',function(){
         number = (number - 1 + this_.song.length) % this_.song.length;
-           const array = this_.song[number]
-           backgroundImg.style.backgroundImage = "url('" + array.lickimg + "')";
+            const array = this_.song[number]
+            backgroundImg.style.backgroundImage = "url('" + array.lickimg + "')";
+            backgroundcolor.style.background = array.backgroundcolor;
+            backgroundcolor.style.backgroundSize = array.backgroundsize;
             nameSong.textContent = array.namesong
             audio.src = array.linkmusic 
             audio.play()
@@ -257,10 +330,16 @@ const app = {
             list_item.classList.add('active_list')
        }
 
+       
+       
+
+       
     },
     loandIndex0:function(){
         const array0 = this.song[0]
         backgroundImg.style.backgroundImage = "url('" + array0.lickimg + "')";
+        backgroundcolor.style.background = array0.backgroundcolor
+        backgroundcolor.style.backgroundSize = array0.backgroundsize
         nameSong.textContent = array0.namesong
         audio.src  = array0.linkmusic
     },
@@ -275,3 +354,6 @@ const app = {
     }
 }
 app.start()
+
+// học cái match để đỏi giây sang phút
+// học cái  number = (number - 1 + this_.song.length) % this_.song.length; // Đảm bảo số luôn dương bằng cách cộng thêm song.length trước khi lấy phần dư
